34fd6050e3357102b11ec0a2b70dce62045ad443
VNGrikke <147390413+VNGrikke@users.noreply.github.com>
2023-11-27 15:36:43 +0700

    Update ss12bt2.c

--- ss12bt2.c
+++ ss12bt2.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void capSo1(int a, int b){
-	printf("cap so thu nhat:\n");
+void capSo(int a, int b){
 	if(a>b){
 		printf("%d * %d = %d\n",a,b,a*b);
 		if(a%b==0){
@@ -19,48 +18,14 @@ void capSo1(int a, int b){
 		}
 	}
 }
-void capSo2(int b, int c){
-	printf("cap so thu hai:\n");
-	if(b>c){
-		printf("%d * %d = %d\n",b,c,b*c);
-		if(b%c==0){
-			printf("%d / %d = %d\n",b,c,b/c);
-		}else{
-			printf("%d khong chia het cho %d\n",b,c);
-		}
-	}else{
-		printf("%d * %d = %d\n",c,b,c*b);
-		if(c%b==0){
-			printf("%d / %d = %d\n",c,b,c/b);
-		}else{
-			printf("%d khong chia het cho %d\n",c,b);
-		}
-	}
-}
-void capSo3(int a, int c){
-	printf("cap so thu ba:\n");
-	if(a>c){
-		printf("%d * %d = %d\n",a,c,a*c);
-		if(a%c==0){
-			printf("%d / %d = %d\n",a,c,a/c);
-		}else{
-			printf("%d khong chia het cho %d\n",a,c);
-		}
-	}else{
-		printf("%d * %d = %d\n",c,a,c*a);
-		if(c%a==0){
-			printf("%d / %d = %d\n",c,a,c/a);
-		}else{
-			printf("%d khong chia het cho %d\n",c,a);
- 		}
-	}
-}
-
 int main(){
 	int a,b,c;
 	printf("nhap vao gia tri a, b, c:");
 	scanf("%d %d %d",&a,&b,&c);
-	capSo1(a,b);
-	capSo2(b,c);
-	capSo3(a,c);
+	printf("cap so thu nhat:\n");
+	capSo(a,b);
+	printf("cap so thu hai:\n");
+	capSo(b,c);
+	printf("cap so thu ba:\n");
+	capSo(a,c);
 }
